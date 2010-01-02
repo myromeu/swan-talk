c1840f57923f3afe88ede189428df74466eaf067
anu.s.varsha <anu.s.varsha@f0e8c9fa-eef6-11de-88d6-afc75ad17f39>
2010-01-02 10:04:41 +0000

    Added Documentation

--- trunk/client/client.py
+++ trunk/client/client.py
@@ -13,13 +13,13 @@ reactor=qt4reactor.install()
 talk_list={}  
 GUI.pushButton_3.setEnabled(False)
 
-class Echo(Protocol):						##to build protocol
+class Echo(Protocol):							##to build protocol
 
-   def dataReceived(self, data):				##called when data is received
+   def dataReceived(self, data):					##called when data is received
 	print "dataaa",data
 	packet=data.split(">>:")
 	if packet[0]=="populate_list":
-		packet.remove('populate_list')			##remove "populate_list" to get user names
+		packet.remove('populate_list')				##remove "populate_list" to get user names
 		GUI.listWidget.clear()
 		j=0
 		
@@ -70,8 +70,8 @@ class Echo(Protocol):						##to build protocol
 			pass
 
 		
-		if packet[2] not in talk_list:			##if user not in user_list
-			current_index=packet[2]			##current_index set to user name
+		if packet[2] not in talk_list:				##if user not in user_list
+			current_index=packet[2]				##current_index set to user name
 			tab = QtGui.QWidget()
         		tab.setObjectName("tab"+packet[2])
         		textBrowser = QtGui.QTextBrowser(tab)
@@ -89,7 +89,7 @@ class Echo(Protocol):						##to build protocol
 			QtCore.QObject.connect(lineEdit,QtCore.SIGNAL("returnPressed()"),Send_Chat)
 			talk_list[packet[2]]=(textBrowser,lineEdit)
 			
-		talk_list[packet[2]][0].append(packet[3]+": "+string)
+		talk_list[packet[2]][0].append(packet[3]+": "+string) 	##to display user name:data in CommonRoom textBrowser
 		
   
 	
@@ -98,13 +98,13 @@ class EchoClientFactory(ClientFactory):
     def startedConnecting(self, connector):
         GUI.setStatusTip(QtGui.QApplication.translate("MainWindow", "Started to connect.", None, QtGui.QApplication.UnicodeUTF8))
     
-    def buildProtocol(self, addr):				##to set protocol as Echo
+    def buildProtocol(self, addr):					##to set protocol as Echo
        
 	GUI.pushButton_3.setEnabled(True)
 	GUI.pushButton_2.setEnabled(False)
         return Echo()
     
-    def clientConnectionLost(self, connector, reason):		##executed when loseConnection() is called
+    def clientConnectionLost(self, connector, reason):			##executed when loseConnection() is called
 	global talk_page
 	GUI.pushButton_3.setEnabled(False)
 	GUI.pushButton_2.setEnabled(True)
@@ -124,7 +124,7 @@ class EchoClientFactory(ClientFactory):
 		pass      
 	 GUI.setStatusTip(QtGui.QApplication.translate("MainWindow", 'Connection Failed, Retry', None, QtGui.QApplication.UnicodeUTF8))
 
-def Send_Details():						##called when chat button is clicked
+def Send_Details():							##called when chat button is clicked
 	global connection,talk_page,talk_list,current_index
 
 	if (GUI.lineEdit.text().__str__().__str__()!=''):
@@ -147,12 +147,12 @@ def Send_Details():						##called when chat button is clicked
 	else:
 		GUI.setStatusTip(QtGui.QApplication.translate("MainWindow", 'Enter your username.', None, QtGui.QApplication.UnicodeUTF8))
 
-def Connect():							##called when connect button is clicked
+def Connect():								##called when connect button is clicked
 	global connection
 	connection=reactor.connectTCP(GUI.lineEdit_2.text().__str__().__str__(),int(GUI.lineEdit_3.text().__str__().__str__()) , EchoClientFactory()) ##listenTCP and connectTCP,to set defaults for all connections coming from that accept() or connect()
 	GUI.lineEdit_3.setEnabled(False)
 	GUI.lineEdit_2.setEnabled(False)
-	GUI.lineEdit.setEnabled(True)				##to connect again from the same window if the user is already logged in
+	GUI.lineEdit.setEnabled(True)					##to connect again from the same window if the user is already logged in
 	GUI.tabWidget.setCurrentIndex(1)
 def Close(index):
 	global talk_page,talk_list
@@ -160,11 +160,11 @@ def Close(index):
 	talk_page.tabWidget.removeTab(index)
 	talk_list.__delitem__(text.__str__().__str__())                                  
 
-def Change_Talk(item):						##to select particular tab for private chat
+def Change_Talk(item):							##to select particular tab for private chat
 	global current_index
 	current_index=item.__str__().__str__()
 
-def New_Talk(item):						##to get a new tab when user name is double clicked for private chat
+def New_Talk(item):							##to get a new tab when user name is double clicked for private chat
 	username=item.text().__str__().__str__().split("(")[0]
 	if username not in talk_list:	
 		tab = QtGui.QWidget()
@@ -184,7 +184,7 @@ def New_Talk(item):						##to get a new tab when user name is double clicked for
 		QtCore.QObject.connect(lineEdit,QtCore.SIGNAL("returnPressed()"),Send_Chat)
 		talk_list[username]=(textBrowser,lineEdit)
 
-def Send_Chat():						##called when returnpressed in lineEdit of talk_Page
+def Send_Chat():							##called when returnpressed in lineEdit of talk_Page
 	global connection,talk_list
 	if current_index != "CommonRoom":
 		talk_list[current_index][0].append(GUI.lineEdit.text().__str__().__str__()+": "+talk_list[current_index][1].text().__str__().__str__()) ##textBrowser(current_index[0]) is given username:content in  lineEdit(current_index[1])
@@ -195,5 +195,5 @@ current_index='CommonRoom'
 QtCore.QObject.connect(GUI.pushButton_3,QtCore.SIGNAL("clicked()"),Send_Details)
 QtCore.QObject.connect(GUI.pushButton_2,QtCore.SIGNAL("clicked()"),Connect)
 QtCore.QObject.connect(GUI.listWidget,QtCore.SIGNAL("itemDoubleClicked(QListWidgetItem*)"),New_Talk)
-reactor.runReturn()						##reactor will run until Ctrl_C is pressed
+reactor.runReturn()							##reactor will run until Ctrl_C is pressed
 sys.exit(app.exec_())
