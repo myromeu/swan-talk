2c30f6a9d22688e5e1f7ab266bf2f980ddd8a8bb
anu.s.varsha <anu.s.varsha@f0e8c9fa-eef6-11de-88d6-afc75ad17f39>
2010-01-04 07:46:35 +0000

    smilies

--- client/client.py
+++ client/client.py
@@ -134,7 +134,7 @@ class Echo(Protocol):							##to build protocol
 				string=string+"<img src=\":/newPrefix/smiley5.jpg\" />"+convers[i]
 		else :
 			pass
-		convers=string.split(";~")
+		convers=string.split(":~")
 		if convers.__len__()>1:
 			string=convers[0]
 			for i in range(1,convers.__len__()):
